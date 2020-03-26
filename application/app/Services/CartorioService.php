2e1834268f7058ee29dce3e6b99a1c6c4b40b49b
paulophmp <paulo.mendes00@hotmail.com>
2020-03-25 22:14:36 -0500

    [UPDATE] Ajustando o a camada de service alterando campos

--- application/app/Services/CartorioService.php
+++ application/app/Services/CartorioService.php
@@ -9,71 +9,74 @@ class CartorioService
 
     public function salvarCartoriosImportXML($request)
     {
-        $clients = simplexml_load_file($request->files->get('file'));
-        $clients = json_decode(json_encode((array) $clients), true);
+        $cartorios = simplexml_load_file($request->files->get('file'));
+        $cartorios = json_decode(json_encode((array) $cartorios), true);
 
-        foreach ($clients['cartorio'] as $client) {
-//        dd($client);
+        foreach ($cartorios['cartorio'] as $cartorio) {
+//        dd($cartorio);
             $arrReplace = [
-                'nome' => $client['nome'],
-                'documento' => $client['documento'],
-                'razao' => $client['razao'],
-                'tipo_documento' => $client['tipo_documento'],
-                'cep' => $client['cep'],
-                'endereco' => $client['endereco'],
-                'tabeliao' => $client['tabeliao'],
-                'bairro' => $client['bairro'],
-                'cidade' => $client['cidade'],
-                'uf' => $client['uf'],
-                'ativo' => $client['ativo'] ? 1 : 0,
-//                'email' => $client['email'],
+                'nome' => $cartorio['nome'],
+                'documento' => $cartorio['documento'],
+                'razao' => $cartorio['razao'],
+                'tipo_documento' => $cartorio['tipo_documento'],
+                'cep' => $cartorio['cep'],
+                'endereco' => $cartorio['endereco'],
+                'tabeliao' => $cartorio['tabeliao'],
+                'bairro' => $cartorio['bairro'],
+                'cidade' => $cartorio['cidade'],
+                'uf' => $cartorio['uf'],
+                'ativo' => $cartorio['ativo'] ? 1 : 0,
+//                'email' => $cartorio['email'],
             ];
             // Verify if document is unique to save only different data
-            $find = Cartorio::where('documento', '=', $client['documento'])->first();
-
-            $client = new Cartorio();
+            $find = Cartorio::where('documento', '=', $cartorio['documento'])->first();
+//dd($find);
+            $cartorio = new Cartorio();
 
             if (!empty($find)) {
-                $client = $client::find($find->id);
+                $cartorio = $cartorio::find($find->id);
             }
 
-            $client->nome = $arrReplace['nome'];
-            $client->documento = $arrReplace['documento'];
-            $client->razao = $arrReplace['razao'];
-            $client->tipo_documento = $arrReplace['tipo_documento'];
-            $client->cep = $arrReplace['cep'];
-            $client->logradouro = $arrReplace['endereco'];
-            $client->nome_tabeliao = $arrReplace['tabeliao'];
-            $client->bairro = $arrReplace['bairro'];
-            $client->localidade = $arrReplace['cidade'];
-            $client->ativo = $arrReplace['ativo'];
-            $client->uf = $arrReplace['uf'];
+            $cartorio->nome = $arrReplace['nome'];
+            $cartorio->documento = $arrReplace['documento'];
+            $cartorio->razao = $arrReplace['razao'];
+            $cartorio->tipo_documento = $arrReplace['tipo_documento'];
+            $cartorio->cep = $arrReplace['cep'];
+            $cartorio->logradouro = $arrReplace['endereco'];
+            $cartorio->nome_tabeliao = $arrReplace['tabeliao'];
+            $cartorio->bairro = $arrReplace['bairro'];
+            $cartorio->localidade = $arrReplace['cidade'];
+            $cartorio->ativo = $arrReplace['ativo'];
+            $cartorio->uf = $arrReplace['uf'];
 
-            $client->save();
+            $cartorio->save();
         }
     }
 
     public function salvarCartorio($request)
     {
 
-            $client = new Cartorio();
-            $client->name = $request['name'];
-            $client->document = $request['document'];
-            $client->postcode = $request['postcode'];
-            $client->address = $request['address'];
-            $client->district = $request['district'];
-            $client->city = $request['city'];
-            $client->active = $request['active'];
-            $client->state = $request['state'];
+            $cartorio = new Cartorio();
+            $cartorio->nome = $request['nome'];
+            $cartorio->documento = $request['documento'];
+            $cartorio->razao = $request['razao'];
+            $cartorio->tipo_documento = $request['tipo_documento'];
+            $cartorio->cep = $request['cep'];
+            $cartorio->logradouro = $request['logradouro'];
+            $cartorio->nome_tabeliao = $request['nome_tabeliao'];
+            $cartorio->bairro = $request['bairro'];
+            $cartorio->localidade = $request['localidade'];
+            $cartorio->ativo = $request['ativo'];
+            $cartorio->uf = $request['uf'];
 
-            $client->save();
+            $cartorio->save();
     }
 
     public function listarCartorio()
     {
-        $clients = Cartorio::orderBy('nome')->get();
+        $cartorios = Cartorio::orderBy('nome')->get();
 
-        return $clients;
+        return $cartorios;
     }
 
     public function editarCartorio($request, $id)
